a2465c3daad136aa9691fb99f35a32d5e9cf5506
NotAsX <106137170+Alekkuntul07@users.noreply.github.com>
2022-05-30 19:44:33 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+Code:
+bot.command({
+    name: "rps",
+    code: `$username choose $message
+the bot choose $randomText[rock;paper;scissors]
+
+you know who win?`
+})
+
+
+you can edit it by urself, customize the code, or make it to embed message.
+btw im not good to make the command xD. i will make some commands if i have idea. see you later
+
+
+
+subscribe my youtube
+https://www.youtube.com/channel/UCXLqf8iEB_fV003L02BSKTQ
